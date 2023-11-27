cfd2c6565b0373025f1f6d8c99ef1003a3502f08
jshtengker <leonardotengker@gmail.com>
2023-11-27 11:53:20 +0800

    about/cta/featuredService

--- src/about/index.jsx
+++ src/about/index.jsx
@@ -8,27 +8,24 @@ const About = () => {
       <div className="container" data-aos="fade-up">
         <div className="section-title">
           <h2>About Us</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>Informasi mengenai U-Clinic..</p>
         </div>
         <div className="row">
           <div className="col-lg-6" data-aos="fade-right">
             <img src="src/assets/img/about.jpg" className="img-fluid" alt />
           </div>
           <div className="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+            <h3>U-Clinic</h3>
             <p className="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+              Klinik Unklab adalah pusat pemeriksaan kesehatan di Universitas Klabat dibawah asuhan Fakultas Keperawatan.
             </p>
             <ul>
-              <li><i className="bi bi-check-circle" /> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i className="bi bi-check-circle" /> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i className="bi bi-check-circle" /> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+              <li><i className="bi bi-check-circle" /> Tenaga medis yang mumpuni.</li>
+              <li><i className="bi bi-check-circle" /> Dokter yang berpengalaman di bidangnya.</li>
+              <li><i className="bi bi-check-circle" /> Berintegritas dan bertugas sesuai Falsafah Universitas Klabat.</li>
             </ul>
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+              Berada di bawah naungan Fakultas Keperawatan Universitas Klabat. Memberikan fasilitas kesehatan untuk seluruh warga Unklab..
             </p>
           </div>
         </div>
