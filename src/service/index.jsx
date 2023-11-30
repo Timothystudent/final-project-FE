44149c5299a39ecb3d3b560e425c1f435dc032bb
jonathanlukass <jhonatanlukas05@gmail.com>
2023-11-30 14:19:42 +0800

    counts/service

--- src/service/index.jsx
+++ src/service/index.jsx
@@ -8,38 +8,38 @@ const Service = () => {
       <div className="container" data-aos="fade-up">
         <div className="section-title">
           <h2>Services</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>Tersedia berbagai macam layanan</p>
         </div>
         <div className="row">
           <div className="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay={100}>
             <div className="icon"><i className="fas fa-heartbeat" /></div>
-            <h4 className="title"><a href>Lorem Ipsum</a></h4>
-            <p className="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+            <h4 className="title"><a href>Pemeriksaan Tekanan Darah</a></h4>
+            <p className="description">Di klinik ini tersedia fasilitas untuk memeriksa tekanan darah.</p>
           </div>
           <div className="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay={200}>
             <div className="icon"><i className="fas fa-pills" /></div>
-            <h4 className="title"><a href>Dolor Sitema</a></h4>
-            <p className="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+            <h4 className="title"><a href>Obat-Obatan</a></h4>
+            <p className="description">Tersedia obat-obatan untuk pasien yang membutuhkan.</p>
           </div>
           <div className="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay={300}>
             <div className="icon"><i className="fas fa-hospital-user" /></div>
-            <h4 className="title"><a href>Sed ut perspiciatis</a></h4>
-            <p className="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+            <h4 className="title"><a href>Konstultasi</a></h4>
+            <p className="description">Di U-klinik tersedi dokter dan dapat melakukan Konstultasi.</p>
           </div>
           <div className="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay={100}>
             <div className="icon"><i className="fas fa-dna" /></div>
-            <h4 className="title"><a href>Magni Dolores</a></h4>
-            <p className="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <h4 className="title"><a href>Pemeriksaan</a></h4>
+            <p className="description">Tersedia pemeriksaan untuk Hepatitis dan TBC.</p>
           </div>
           <div className="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay={200}>
             <div className="icon"><i className="fas fa-wheelchair" /></div>
-            <h4 className="title"><a href>Nemo Enim</a></h4>
-            <p className="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+            <h4 className="title"><a href>Fasilitas</a></h4>
+            <p className="description">Tersedia berbagai macam fasilitas yang lumayan lengkap.</p>
           </div>
           <div className="col-lg-4 col-md-6 icon-box" data-aos="zoom-in" data-aos-delay={300}>
             <div className="icon"><i className="fas fa-notes-medical" /></div>
-            <h4 className="title"><a href>Eiusmod Tempor</a></h4>
-            <p className="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+            <h4 className="title"><a href>Penanganan Darurat</a></h4>
+            <p className="description">Menangani pasien secara sigap.</p>
           </div>
         </div>
       </div>
