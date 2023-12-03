b438e27252f59a65545fc4b6afa482f7b53e9536
Timothystudent <s22110621@student.unklab.ac.id>
2023-12-04 01:23:17 +0800

    update

--- src/homepage/index.jsx
+++ src/homepage/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 const Homepage = () => {
-  return (
-    <>
-<div>
-  {/* ======= Top Bar ======= */}
-  <main id="main">
-  </main>{/* End #main */}
-  {/* ======= Footer ======= */}
+//   return (
+//     <>
+// <div>
+//   {/* ======= Top Bar ======= */}
+//   <main id="main">
+//   </main>{/* End #main */}
+//   {/* ======= Footer ======= */}
   
-  {/* <div id="preloader" />
-  <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short" /></a> */}
-</div>
-    </>
-  )
+//   {/* <div id="preloader" />
+//   <a href="#" className="back-to-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short" /></a> */}
+// </div>
+//     </>
+//   )
 }
 
 export default Homepage
