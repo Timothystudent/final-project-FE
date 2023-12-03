fc9623f8d97a38026a5687c05d2373ca101bdc7d
Timothystudent <s22110621@student.unklab.ac.id>
2023-12-04 01:35:52 +0800

    updatee

--- src/cta/index.jsx
+++ src/cta/index.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const Cta = () => {
   return (
     <>
-    {/* ======= Cta Section ======= */}
-    <section id="cta" className="cta">
-      <div className="container" data-aos="zoom-in">
-        <div className="text-center">
-          <h3>Mempunyai masalah?</h3>
-          <p>Kesehatan terganggu? Sedang merasakan sesuatu tapi tidak mengetahui penyebabnya? Ingin memastikan kondisi anda? Silahkan membuat appointment.. </p>
-          <a className="cta-btn scrollto" href="#appointment">Make an Appointment</a>
+      {/* ======= Cta Section ======= */}
+      <section id="cta" className="cta">
+        <div className="container" data-aos="zoom-in">
+          <div className="text-center">
+            <h3>Mempunyai masalah?</h3>
+            <p>
+              Kesehatan terganggu? Sedang merasakan sesuatu tapi tidak mengetahui penyebabnya? Ingin memastikan kondisi anda? Silahkan membuat appointment..
+            </p>
+            <a className="cta-btn scrollto" href="#appointment">
+              Make an Appointment
+            </a>
+          </div>
         </div>
-      </div>
-    </section>{/* End Cta Section */}
+      </section>
+      {/* End Cta Section */}
     </>
-  )
-}
+  );
+};
 
-export default Cta
+export default Cta;
