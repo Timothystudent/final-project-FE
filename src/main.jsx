8b15d62aaa7b45e1f5c3301e3a7acee04aafb0d1
Timothystudent <s22110621@student.unklab.ac.id>
2023-12-04 09:21:04 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import '/src/assets/vendor/animate.css/animate.min.css'
+import '/src/assets/vendor/aos/aos.css'
+import '/src/assets/vendor/bootstrap/css/bootstrap.min.css'
+import '/src/assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import '/src/assets/vendor/boxicons/css/boxicons.min.css'
+import '/src/assets/vendor/glightbox/css/glightbox.min.css'
+import '/src/assets/vendor/swiper/swiper-bundle.min.css'
+import '/src/assets/vendor/fontawesome-free/css/all.min.css'
+import '/src/assets/css/style.css'
+
+// Import scripts
+
+
+// Initialize other scripts as needed
+
+// Your Vite application code here
+
+// import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
